435ff38719390050c73ec9eee433052a9b3b9666
jagritx <jagritkamra@gmail.com>
2021-09-13 21:02:34 +0530

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+function Header() {
+  return (
+    <header>
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow flex-sm-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={50}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+          <div className="hidden sm:flex items-center rounded-md bg-yellow-400 hover:bg-yellow-500 cursor-pointer flex-grow">
+            <input
+              className="p-3 h-full w-8 flex-grow flex-shrink rounded-l-md"
+              type="text"
+            />
+            <SearchIcon className="h-12 p-4 focus:outline-none" />
+          </div>
+          <div className="text-white flex items-center text-xs space-x-6 m-6 whitespace-nowrap">
+            <div className="link">
+              <p>Hello ZedddX!</p>
+              <p className="font-extrabold md:text-sm">Account and Lists</p>
+            </div>
+            <div className="link">
+              <p>Returns</p>
+              <p className="font-extrabold md:text-sm">& Orders</p>
+            </div>
+            <div className=" relative link flex items-center">
+              <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+                0
+              </span>
+              <ShoppingCartIcon className="h-10" />
+              <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+                Basket
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
