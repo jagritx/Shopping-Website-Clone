04b153b53829c268396459d2966dee89a3e43bfb
jagritx <jagritkamra@gmail.com>
2021-09-21 21:15:21 +0530

    Added Add to Basket in Main component

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,11 +2,27 @@ import Image from "next/image";
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
 function Product({ id, title, price, description, category, image }) {
+  const dispatch = useDispatch();
   const [rating] = useState(Math.floor(Math.random() * 5) + 1);
 
   const [hasPrime] = useState(Math.random() < 0.5);
+
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+    };
+    dispatch(addToBasket(product));
+  };
+
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">
@@ -32,7 +48,9 @@ function Product({ id, title, price, description, category, image }) {
           <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
       )}
-      <button className=" mt-auto button">Add to Basket</button>
+      <button onClick={addItemToBasket} className=" mt-auto button">
+        Add to Basket
+      </button>
     </div>
   );
 }
