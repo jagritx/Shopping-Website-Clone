a7fb94921c86bffbfb38f167467b6084cb9553e1
jagritx <jagritkamra@gmail.com>
2021-09-21 21:27:59 +0530

    Minor Design Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,45 +14,59 @@ function Header() {
   const router = useRouter();
   const items = useSelector(selectItems);
   return (
-    <header>
-      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2 ">
-        <div className="mt-2 flex items-center flex-grow flex-sm-grow-0">
+    <header className="sticky top-0 z-50">
+      {/* Top nav */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
-            height={50}
+            height={40}
             objectFit="contain"
-            className="cursor-pointer"
+            className="cursor-pointer active:transform active:scale-90"
           />
-          <div className="hidden sm:flex items-center rounded-md bg-yellow-400 hover:bg-yellow-500 cursor-pointer flex-grow">
-            <input
-              className="p-3 h-full w-8 flex-grow flex-shrink rounded-l-md"
-              type="text"
-            />
-            <SearchIcon className="h-12 p-4 focus:outline-none" />
+        </div>
+
+        {/* Custom search bar */}
+        <div className="hidden sm:flex items-center h-10 rounded-md bg-yellow-400 hover:bg-yellow-500 flex-grow cursor-pointer">
+          <input
+            type="text"
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/* Right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div
+            onClick={!session ? signIn : signOut}
+            className="link cursor-pointer"
+          >
+            <p className="hover:underline">
+              {session ? `Hello, ${session.user.name}` : "Sign In"}
+            </p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
-          <div className="text-white flex items-center text-xs space-x-6 m-6 whitespace-nowrap">
-            <div onClick={!session ? signIn : signOut} className="link">
-              <p>{session ? `Hello,${session.user.name}` : "Sign In"}</p>
-              <p className="font-extrabold md:text-sm">Account and Lists</p>
-            </div>
-            <div className="link">
-              <p>Returns</p>
-              <p className="font-extrabold md:text-sm">& Orders</p>
-            </div>
-            <div
-              className=" relative link flex items-center"
-              onClick={() => router.push("/checkout")}
+          <div className="link" onClick={() => router.push("/orders")}>
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div
+            className="relative link flex items-center"
+            onClick={() => router.push("/checkout")}
+          >
+            <span
+              className={`absolute top-0 right-0 md:right-10 h-4 ${
+                items.length >= 10 ? "w-6" : "w-4"
+              } bg-yellow-400 text-center rounded-full text-black font-bold`}
             >
-              <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-                {items.length}
-              </span>
-              <ShoppingCartIcon className="h-10" />
-              <p className="hidden md:inline font-extrabold md:text-sm mt-2">
-                Basket
-              </p>
-            </div>
+              {items.length}
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
           </div>
         </div>
       </div>
