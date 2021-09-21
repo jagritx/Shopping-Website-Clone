e99e975516cea059e78da30e925749c8d85d749e
jagritx <jagritkamra@gmail.com>
2021-09-21 21:14:48 +0530

    Added Item count in badge icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,13 @@ import {
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/client";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 function Header() {
   const [session] = useSession();
   const router = useRouter();
+  const items = useSelector(selectItems);
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2 ">
@@ -43,7 +46,7 @@ function Header() {
               onClick={() => router.push("/checkout")}
             >
               <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-                0
+                {items.length}
               </span>
               <ShoppingCartIcon className="h-10" />
               <p className="hidden md:inline font-extrabold md:text-sm mt-2">
